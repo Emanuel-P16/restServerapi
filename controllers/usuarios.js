07a613939696a02e6341bc9208bd9385a5ddc580
Emanuel-P16 <palazon.emanuel@gmail.com>
2022-02-08 19:02:39 -0300

    se agrego el GET

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -8,8 +8,24 @@ const usuariosGet  = async(req = request,res = response)=> {
 
     // const query = req.query;
     // const { q,nombre = 'no name', page = '1', apikey} = req.query
-    const usuarios = await Usuario.find()
+    const { limite = 5,desde = 0} = req.query;
+    const query = { estado: true}
+    // const usuarios = await Usuario.find(query)
+    //     .skip(Number(desde))
+    //     .limit(Number(limite))
+
+    // const total = await Usuario.countDocuments(query);
+
+    const [total,usuarios] = await Promise.all([
+        Usuario.count(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ]);
+
+
     res.json({
+        total,
         usuarios
     })
 }

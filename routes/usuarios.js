f93433264c0e72c08a180af4ee340ba0d21b0a70
Emanuel-P16 <palazon.emanuel@gmail.com>
2022-03-08 11:54:52 -0300

    se agregaron validaciones de usuario al eliminar

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,8 +3,15 @@ const { Router } = require('express');
 const { check } = require('express-validator')
 
 
-const { validarCampos } = require('../middlewares/validar-campos')
-const { validarJWT } = require('../middlewares/validar-jwt');
+// const { validarCampos } = require('../middlewares/validar-campos')
+// const { validarJWT } = require('../middlewares/validar-jwt');
+// const { esAdminRole, tieneRole } = require('../middlewares/validar-roles');
+
+const { validarCampos,
+        validarJWT,
+        esAdminRole,
+        tieneRole 
+    } = require('../middlewares')
 
 const { esRoleValido, esMailValido,existeUsuarioPorId } = require('../helpers/db-validators');
 
@@ -34,6 +41,8 @@ const router = Router();
     ],usuariosPost)
     router.delete('/:id',[
         validarJWT,
+        // esAdminRole,
+        tieneRole('ADMIN_ROLE','VENTAS_ROLE','USER_ROLE'),
         check('id', "No es un ID válido").isMongoId(),
         check('id').custom(existeUsuarioPorId),
     ],usuariosDelete)
